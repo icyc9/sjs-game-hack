b874cbc816c71ddcbee48d2f48a55701530de77f
Robert Christopher <robertc@Roberts-Air.fios-router.home>
2015-12-16 19:15:42 -0500

    add sjs.js

---
+++ sjs.js
@@ -0,0 +1,13 @@
+"undefined" == typeof sjsHack && (window.sjsHack = {}),
+    function(s) {
+        var i = window;
+        this.getToken = function() {
+            return i.s(this.email, this.score, i.token)
+        }, this.hack = function(s, i) {
+            this.score = i, this.email = s, $.ajax({
+                url: "http://www.slamjamsocialism.com/arcad-ism/callback-ism.php",
+                data: "action=setpoint&email=" + s + "&points=" + i + "&sec=" + this.getToken(),
+                type: "POST"
+            })
+        }
+    }.call(sjsHack);
